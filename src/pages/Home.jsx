17a83f2ef43253865eb105d159064e33e4da62b0
HaiQuan21 <phamhaiquan22112004@gmail.com>
2025-06-05 23:42:55 +0700

    Update Signup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      HELLLO IT ME PHYGEN
+    <div className="min-h-screen flex items-center justify-center bg-[#f5f9ff]">
+      <h1 className="text-6xl font-bold text-[#004AAD] text-center">
+        HELLO, THIS IS PHYGEN
+      </h1>
     </div>
   )
 }
