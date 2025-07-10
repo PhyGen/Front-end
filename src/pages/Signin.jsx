1d50f642dc8663679a3c2b5888b007c002f69163
HaiQuan21 <phamhaiquan22112004@gmail.com>
2025-07-10 21:57:37 +0700

    Push all

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -75,7 +75,7 @@ const SignIn = () => {
     try {
       setIsLoading(true);
       setError('');
-      
+
       console.log('Google login response:', response);
       
       // Gửi credential lên backend (backend mong đợi field 'credential')
